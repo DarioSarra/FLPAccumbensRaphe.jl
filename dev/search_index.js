f3ab76e8fce3373e38c92b52157673e6290900c3
Documenter.jl <documenter@juliadocs.github.io>
2023-09-04 12:57:08 +0000

    build based on 1dcbf04

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FLPAccumbensRaphe","category":"page"},{"location":"#FLPAccumbensRaphe","page":"Home","title":"FLPAccumbensRaphe","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FLPAccumbensRaphe.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FLPAccumbensRaphe]","category":"page"}]
+}
